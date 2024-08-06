c3ee39ffff952c34b79ba5d5e897a19fd2d03ebb
ThanawitGZS <Tanawit5578@gmail.com>
2024-08-06 22:41:46 +0700

    Create employee.go

---
+++ Backend/employee.go
@@ -0,0 +1,44 @@
+package entity
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Employee struct {
+
+	EmployeeID   string    `gorm:"primaryKey"  json:"employee_id"`
+
+    FirstName    string    `json:"first_name"`
+
+    LastName     string    `json:"last_name"`
+
+    Username     string    `json:"username"`
+
+    Password     string    `json:"password"`
+
+    RegisterDate time.Time `json:"register_date"`
+
+	
+	GenderID     *string   
+
+	Gender		Gender     `gorm:"foriegnKey:gender_id"`
+
+	PositionID   *string   
+
+	Position	Position	`gorm:"foriegnKey:position_id"`
+
+	Member []Member `gorm:"foriegnKey:employee_id"`
+
+	Receipt []Receipt `gorm:"foriegnKey:employee_id"`
+
+	// เรากำหนด primaryKey  type เองเลยไม่ได้ใช้ gorm.model
+
+	CreatedAt time.Time
+
+    UpdatedAt time.Time
+
+    DeletedAt gorm.DeletedAt `gorm:"index"`
+
+}
