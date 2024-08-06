e6e714d261c4fc9b27fb38177c59f305bcc287cf
ThanawitGZS <Tanawit5578@gmail.com>
2024-08-06 22:41:51 +0700

    Create position.go

---
+++ Backend/position.go
@@ -0,0 +1,17 @@
+package entity
+
+
+import (
+	"gorm.io/gorm"
+)
+
+type Position struct {
+
+    gorm.Model
+
+    PositionName       string `json:"position_name"`
+
+     // Position 0..* to 1 Employee
+    Employee []Employee `gorm:"foreignKey:PositionID"`
+
+}
